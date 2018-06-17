ab65f0dc7397eaccaa1abb552b96cf24ead72d07
pophiphip <pophiphop@ship.com>
2018-06-17 13:49:57 +0900

    deleted cycle.js and wrote codes to cycle texts in sketch_top.js

--- resources/views/octopaths/index.blade.php
+++ resources/views/octopaths/index.blade.php
@@ -8,7 +8,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.6.1/p5.min.js"></script>
     <script>var assetBasePath = "{{ asset('') }}"; //used in sketch_top.js to anchor img files</script>
     <script src="{{ asset('js/sketch_top.js') }}"></script>
-    {{-- <script src="{{ asset('js/cycleText.js') }}"></script> --}}
     <link rel="stylesheet" href="{{ asset('css/index.css') }}">
 @endsection
 
